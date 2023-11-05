d610651a76d1eea3c46b78fc45579a328c5fc623
Ira Hopkinson <ira_hopkinson@sil.org>
2023-11-06 11:19:07 +1300

    rename internal react package
    - name conflicts with external package causing dependency issues

--- packages/perf-react/jest.config.ts
+++ packages/perf-react/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
-  displayName: "react",
+  displayName: "perf-react",
   preset: "../../jest.preset.js",
   testEnvironment: "node",
   transform: {
     "^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }],
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "html"],
-  coverageDirectory: "../../coverage/packages/react",
+  coverageDirectory: "../../coverage/packages/perf-react",
 };
--- packages/perf-vanilla/jest.config.ts
+++ packages/perf-vanilla/jest.config.ts
@@ -1,10 +1,10 @@
 export default {
-  displayName: "vanilla",
+  displayName: "perf-vanilla",
   preset: "../../jest.preset.js",
   testEnvironment: "node",
   transform: {
     "^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }],
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "html"],
-  coverageDirectory: "../../coverage/packages/vanilla",
+  coverageDirectory: "../../coverage/packages/perf-vanilla",
 };
