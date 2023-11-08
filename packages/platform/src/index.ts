760199f61c0c3e17370bd956afd16616d75c5953
Ira Hopkinson <ira_hopkinson@sil.org>
2023-11-09 07:26:03 +1300

    build platform for production

---
+++ packages/platform/src/index.ts
@@ -0,0 +1,3 @@
+export { default as Editor } from "./editor/Editor";
+export { type Usj } from "./editor/converters/usj.model";
+export { usxStringToJson } from "./editor/converters/usx-to-usj";
--- packages/platform/vite.config.ts
+++ packages/platform/vite.config.ts
@@ -1,7 +1,26 @@
+import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ rollupTypes: true, exclude: ["src/App.tsx", "src/main.tsx"] })],
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
+      fileName: "index",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
 });
